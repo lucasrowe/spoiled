4cf9a0f635b0c61c2e8abd5fd18014f173979f3f
Lucas Rowe <lucas.rowe@gmail.com>
2015-11-11 02:45:18 -0500

    Search now compares against terms in local storage. Can only add terms.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,15 @@
+chrome.storage.local.get(['spoilerterms'], function(result) {
+  if (!result.spoilerterms)
+    return;
 
-var items = document.getElementsByTagName("p");
-for (var i = items.length; i--;) {
-    //do stuff
-    if (items[i].innerHTML.indexOf("the") != -1) {
-      items[i].className += " hidden-spoiler";
-      items[i].innerHTML = "TEXT OVERRIDDEN BY SPOILER SPOILER";
+  var items = document.getElementsByTagName("p");
+  for (var i = items.length; i--;) {
+
+    for (var j = 0; j < result.spoilerterms.length; j++) {
+      if (items[i].innerHTML.indexOf(result.spoilerterms[j]) != -1) {
+        items[i].className += " hidden-spoiler";
+        items[i].innerHTML = "TEXT OVERRIDDEN BY SPOILER SPOILER";
+      }
     }
-}
+  }
+});
--- popup.js
+++ popup.js
@@ -1,3 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
+console.log ("POPUP SCRIPT LOADED");
+
+// For now use local but eventually we'll want some syncing
+var storage = chrome.storage.local;
+var terms = [];
+
+function submitSpoiler(submitButton) {
+  if (window.localStorage == null) {
+    alert('Local storage is required for changing providers');
+    return;
+  }
+  // Save it using the Chrome extension storage API.
+  var newTerm = document.getElementById('spoiler-textfield').value;
+  terms.push(newTerm);
+  storage.set({'spoilerterms': terms}, function() {
+    generateTermsList (terms);
+    document.getElementById('spoiler-textfield').value = "";
+  });
+}
+
+function getSpoilerTerms() {
+  storage.get(['spoilerterms'], function(result) {
+      // Nothing to change.
+      if (!result.spoilerterms)
+        return;
+
+      terms = result.spoilerterms;
+      generateTermsList (terms);
+
+      console.log (result.spoilerterms);
+    });
+}
+
+function generateTermsList(terms) {
+  var oldList = document.getElementById("spoiler-list");
+  if (oldList) {
+    oldList.remove();
+  }
+
+  var container = document.getElementById("spoiler-list-container");
+  var newList = document.createElement('ul');
+  newList.setAttribute ("id", "spoiler-list");
+  container.appendChild (newList);
+
+  for (var i = 0; i < terms.length; i++) {
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(terms[i]));
+    newList.appendChild(entry);
+  }
+}
+
+function main() {
+  if (window.localStorage == null) {
+    alert("LocalStorage must be enabled for changing options.");
+    document.getElementById('spoiler-textfield').disabled = true;
+    document.getElementById('submit-btn').disabled = true;
+    return;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  main();
+  getSpoilerTerms ();
+  document.querySelector('#submit-btn').addEventListener('click', submitSpoiler);
 });
