42ba652131fb54a4b1ab638416a7aaab73c95d7f
Lucas Rowe <lucas.rowe@gmail.com>
2015-11-13 00:34:03 -0500

    Added link and header searching. Removed reference to Spoiler Spoiler.

--- content_script.js
+++ content_script.js
@@ -3,13 +3,27 @@ chrome.storage.local.get(['spoilerterms'], function(result) {
     return;
 
   var items = document.getElementsByTagName("p");
-  for (var i = items.length; i--;) {
+  replaceItemsWithMatchingText (items, result.spoilerterms, "[text overridden by Spoiled]");
 
-    for (var j = 0; j < result.spoilerterms.length; j++) {
-      if (items[i].innerHTML.indexOf(result.spoilerterms[j]) != -1) {
+  items = document.getElementsByTagName("a");
+  replaceItemsWithMatchingText (items, result.spoilerterms, "[link overridden by Spoiled]");
+
+  items = document.getElementsByTagName("h1");
+  items.append (document.getElementsByTagName("h2"));
+  items.append (document.getElementsByTagName("h3"));
+  items.append (document.getElementsByTagName("h4"));
+  items.append (document.getElementsByTagName("h5"));
+  replaceItemsWithMatchingText (items, result.spoilerterms, "[text overridden by Spoiled]");
+});
+
+function replaceItemsWithMatchingText(items, spoilerTerms, replaceString)
+{
+  for (var i = items.length; i--;) {
+    for (var j = 0; j < spoilerTerms.length; j++) {
+      if (items[i].innerHTML.indexOf(spoilerTerms[j]) != -1) {
         items[i].className += " hidden-spoiler";
-        items[i].innerHTML = "TEXT OVERRIDDEN BY SPOILER SPOILER";
+        items[i].innerHTML = replaceString;
       }
     }
   }
-});
+}
