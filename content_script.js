f8bbd21f7197badea97a2f7423c606614aab7ac7
Lucas Rowe <lucas.rowe@gmail.com>
2015-11-17 02:29:54 -0500

    Divs and spans with text inside are now caught. This was widely seen on Facebook.

--- content_script.js
+++ content_script.js
@@ -1,17 +1,24 @@
 var cachedTerms = []
 var elementsWithInnerHTMLToSearch = "a, p, h1, h2, h3, h4, h5, h6, i, em, strong";
+var containerElements = "span, div, li, th, td, dt, dd";
 
 chrome.storage.local.get(['spoilerterms'], function(result) {
   if (!result.spoilerterms)
     return;
   cachedTerms = result.spoilerterms;
 
+  // Search innerHTML elements first
   items = document.querySelectorAll(elementsWithInnerHTMLToSearch)
   replaceItemsWithMatchingText (items, result.spoilerterms, "[text overridden by Spoiled]");
+
+  // Now find any container elements that have just text inside them
+  items = findContainersWithTextInside (document);
+  if (items && items.length != 0) {
+    replaceItemsWithMatchingText (items, result.spoilerterms, "[text overridden by Spoiled]");
+  }
 });
 
-function replaceItemsWithMatchingText(items, spoilerTerms, replaceString)
-{
+function replaceItemsWithMatchingText(items, spoilerTerms, replaceString) {
   for (var i = items.length; i--;) {
     for (var j = 0; j < spoilerTerms.length; j++) {
       var regex = new RegExp(spoilerTerms[j], "i");
@@ -22,6 +29,17 @@ function replaceItemsWithMatchingText(items, spoilerTerms, replaceString)
       }
     }
   }
+}
+
+function findContainersWithTextInside (target) {
+  var containerNodes = target.querySelectorAll(containerElements);
+  var emptyNodes = [];
+  for (var i = 0; i < containerNodes.length; i++) {
+    if (containerNodes[i].childNodes.length == 0 || containerNodes[i].childNodes[0].nodeType == 3) {
+      emptyNodes.push(containerNodes[i]);
+    }
+  }
+  return emptyNodes;
 }
 
 // Detecting changed content using Mutation Observers
@@ -35,7 +53,10 @@ var observer = new MutationObserver(function(mutations, observer) {
     // console.log(mutations, observer);
     for (var i = 0; i < mutations.length; i++) {
       var newNodes = mutations[i].target.querySelectorAll (elementsWithInnerHTMLToSearch);
-      replaceItemsWithMatchingText (newNodes, cachedTerms, "CAUGHT AND HIDDEN");
+      replaceItemsWithMatchingText (newNodes, cachedTerms, "[text overridden by Spoiled]");
+
+      newNodes = findContainersWithTextInside (mutations[i].target);
+      replaceItemsWithMatchingText (newNodes, cachedTerms, "[text overridden by Spoiled]");
     }
 });
 
